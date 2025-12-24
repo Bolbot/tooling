d3005f6036f532c5d3fa38a8ece091cecd717c22
Bolbot <Bolbot2018@outlook.com>
2025-12-24 05:38:51 +0200

    bootstrap: act as a submodule

--- .justfile
+++ .justfile
@@ -9,7 +9,7 @@ alias t := teardown
 # fresh ubuntu might miss python, but not python3, so we try both
 [doc("Runs bootstrap.py script to set up all necessary tooling")]
 setup:
-    python bootstrap.py || python3 bootstrap.py
+    python tooling/bootstrap.py || python3 tooling/bootstrap.py
 
 [doc("Removes the entire tooling; last resort for troubleshooting")]
 teardown:
--- bootstrap.py
+++ bootstrap.py
@@ -6,22 +6,42 @@ import sys
 import subprocess
 import urllib.request
 import os
+import shutil
 
-RED   : Final = "\033[31m"
-GREEN : Final = "\033[32m"
-RESET : Final = "\033[0m"
 
-tooling_path: Final = Path(__file__).parent.absolute() / ".tools"
-venv_path   : Final = Path(__file__).parent.absolute() / ".venv"
+RED    : Final = "\033[31m"
+YELLOW : Final = "\033[33m"
+GREEN  : Final = "\033[32m"
+RESET  : Final = "\033[0m"
+
+main_project: Final = Path(__file__).parent.absolute().parent
+tooling_path: Final = main_project / ".tools"
+venv_path   : Final = main_project / ".venv"
 venv_python : Final = venv_path / "Scripts/python.exe" if sys.platform == "win32" else venv_path / "bin/python"
+
+
+def propagate_justfile():
+    justfile_source = Path(__file__).resolve().parent / ".justfile"
+
+    main_justfile = main_project / ".justfile"
+    if not main_justfile.exists():
+        print(f"Adding {GREEN}.justfile{RESET} to {main_project}\n"\
+            f"Don't forget to add it to your VCS (e. g. {GREEN}git add .justfile{RESET})\n"\
+            "Usage:\n\tjust --list\n")
+        shutil.copyfile(str(justfile_source), str(main_justfile))
+    else:
+        print(f"{YELLOW}Found .justfile in {main_project}{RESET}\nExamine tooling/.justfile to verify yours is relevant")
+
 
 def running_in_native_venv() -> bool:
     if sys.prefix == sys.base_prefix:
-        print(f"Not running in virtual environment")
+        print(f"{YELLOW}Not running in virtual environment{RESET}")
         return False
     if Path(sys.executable).absolute() != venv_python.absolute():
+        print(f"{RED}Running in unrelated virtual environment{RESET}\n\texpected:  {venv_python}\n\tactual:    {str(sys.executable)}")
         return False
     return True
+
 
 def prime_uv():
     uv_path = tooling_path / "uv" / ("uv.exe" if sys.platform == "win32" else "uv")
@@ -65,6 +85,7 @@ def prime_uv():
 
     return uv_path
 
+
 def get_activation_hint():
     if sys.platform == "win32":
         return "cmd:\t\t.venv\\Scripts\\activate\n"\
@@ -73,25 +94,35 @@ def get_activation_hint():
     else:
         return "source .venv/bin/activate"
 
+
+def prime_requirements():
+    fallback_requirements = Path(__file__).resolve().parent / "requirements.txt"
+
+    requirements_path = main_project / "requirements.txt"
+    if not requirements_path.exists():
+        print(f"{YELLOW}No requirements.txt found in {str(main_project)}{RESET}\n"\
+            f"Default {GREEN}requirements.txt{RESET} is provided by bootstrap script.\n"\
+            f"Don't forget to add it to your project VCS (e. g. {GREEN}git add requirements.txt{RESET})\n")
+        shutil.copyfile(str(fallback_requirements), str(requirements_path))
+
+    return requirements_path
+
+
 def main():
     print(f"Running bootstrap script {__file__}")
+
+    propagate_justfile()
 
     local_uv = prime_uv()
     requires_activation = not running_in_native_venv()
     if requires_activation:
-        if venv_python.exists():
-            print(f"{RED}Not running in native virtual environment{RESET}\n  expected:  {venv_python}\n  actual:    {str(sys.executable)}")
-        else:
-            subprocess.run([local_uv, "venv"], check=True)
-            subprocess.run([local_uv, "pip", "install", "--upgrade", "pip"], check=True)
-        print(f"Activate your shell with the appropriate command\n{get_activation_hint()}")
-        if sys.platform == "win32":
-            print(f"\n! Always execute this script as {GREEN}python3 bootstrap.py\n{RESET}")
+        if not venv_python.exists():
+            subprocess.run([local_uv, "venv"], check=True, cwd=main_project)
+            subprocess.run([local_uv, "pip", "install", "--upgrade", "pip"], check=True, cwd=main_project)
 
-    requirements_path = Path(__file__).parent.resolve() / "requirements.txt"
-    if requirements_path.exists():
-        print(f"Adding the requirements from {str(requirements_path)}")
-        subprocess.run([local_uv, "pip", "install", "-r", str(requirements_path)], check=True)
+    requirements_path = prime_requirements()
+    print(f"Adding the requirements from {str(requirements_path)}")
+    subprocess.run([local_uv, "pip", "install", "-r", str(requirements_path)], check=True, cwd=main_project)
 
     if requires_activation:
         print(f"\n\nDon't forget to activate your virtual environment:{GREEN}\n{get_activation_hint()}{RESET}")
